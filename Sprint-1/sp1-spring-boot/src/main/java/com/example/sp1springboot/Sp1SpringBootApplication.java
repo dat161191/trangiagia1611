4b26af1738cc8e93311de192cd9105a0a06aff54
TranGia <quocdat.tran453@gmail.com>
2023-01-19 01:18:10 +0700

    first commit

---
+++ Sprint-1/sp1-spring-boot/src/main/java/com/example/sp1springboot/Sp1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sp1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp1SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp1SpringBootApplication.class, args);
+	}
+
+}
---
+++ Sprint-1/sp1-spring-boot/src/test/java/com/example/sp1springboot/Sp1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sp1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp1SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
