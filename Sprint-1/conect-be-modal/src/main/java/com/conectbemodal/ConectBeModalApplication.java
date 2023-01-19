8657504c34fbd6256fada3d0ad8c844de3a39bbd
TranGia <quocdat.tran453@gmail.com>
2023-01-19 21:43:08 +0700

    first commit

---
+++ Sprint-1/conect-be-modal/src/main/java/com/conectbemodal/ConectBeModalApplication.java
@@ -0,0 +1,13 @@
+package com.conectbemodal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectBeModalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectBeModalApplication.class, args);
+    }
+
+}
---
+++ Sprint-1/conect-be-modal/src/test/java/com/conectbemodal/ConectBeModalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectbemodal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectBeModalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
